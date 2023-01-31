91dcea4e159ae511a5f278fdd74b2c981dbe07da
SigningOwl <whddms1208@gmail.com>
2023-02-01 00:08:37 +0900

    make account package

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"accounts"
+
+	"fmt"
+)
+
+func main() {
+	account := accounts.NewAccount("JongEun") //account 초기화
+	fmt.Println(account)
+}
--- something/something.go
+++ something/something.go
@@ -2,10 +2,10 @@ package something
 
 import "fmt"
 
-func sayBye() {
+func sayBye() { //외부 package의 method 이름이 소문자로 시작하면 private하게 동작하므로 외부에서 접근할 수 없다.
 	fmt.Println("Bye")
 }
 
-func SayHello() {
+func SayHello() { //외부 package로 export를 해서 사용하기 위해서는 method의 이름이나 struct의 이름, 멤버의 이름이 대문자로 시작하여 public으로 동작하도록 해야한다.
 	fmt.Println("Hello")
 }
