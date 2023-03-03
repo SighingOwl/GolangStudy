8ecd0207f1faa8a2913b3612688bbac91b433279
SigningOwl <whddms1208@gmail.com>
2023-03-03 17:26:08 +0900

    Practice channel at main.go

--- main.go
+++ main.go
@@ -84,10 +84,22 @@ func main() {
 		//go sexyCount("Porsche") //Goroutine은 main 함수가 실행되는 동안에만 실행된다. main 함수에서 호출하는 모든 함수에 "go"를 사용하면 모든 Goroutine을 동시에 실행하고 끝이 나버린다. main 함수는 Goroutine이 종료될 때까지 기다리지 않아서 그대로 main 함수가 종료된다.
 		sexyCount("Porsche") // 앞에 "go"가 없는 이 함수가 위의 함수와 동시에 실행될 수 있는 이유는 main함수가 이 함수를 카운팅하고 있기 때문이다.
 	*/
-	go sexyCount("JE")
-	go sexyCount("Porsche")
-	time.Sleep(time.Second * 5) // main 함수가 종료되지 않도록 하는 장치가 있다면 위의 예시처럼 해도 된다.
+	/*
+		go sexyCount("JE")
+		go sexyCount("Porsche")
+		time.Sleep(time.Second * 5) // main 함수가 종료되지 않도록 하는 장치가 있다면 위의 예시처럼 해도 된다.
+	*/
 
+	//channel
+	c := make(chan bool) // channel을 만들때 make(chan "type")을 사용한다.
+	people := [2]string{"JE", "William"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	result := <-c // channel의 메세지를 받는 방법, channel을 통해 메세지를 받을 때 main 함수는 어떤 답이 올때까지 기다리므로 main 함수는 종료되지 않는다.
+	fmt.Println(result)
+	fmt.Println(<-c) // 이렇게 해도 된다.
+	fmt.Println(<-c) // 코드에서 실제로 동작하고 있는 Goroutine은 2개여서 channel에서 메세지를 받을 수 있지만 마지막 하나는 메세지를 계속 기다리게 되서 deadlock이 발생한다.
 }
 
 func sexyCount(person string) {
@@ -95,4 +107,10 @@ func sexyCount(person string) {
 		fmt.Println(person, "is sexy", i)
 		time.Sleep(time.Second)
 	}
+}
+
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	fmt.Println(person)
+	c <- true //channel로 값을 보내고 싶을 때 chan 인자에 <-를 사용해서 값을 지정한다.
 }
